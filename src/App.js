56ae68cda1bec0288357aaaac9976c6f7845a472
Cowboy Ed <eaflores1231@gmail.com>
2021-12-24 07:27:00 -0500

    new fetch

--- src/App.js
+++ src/App.js
@@ -13,20 +13,29 @@ function App() {
 
   useEffect(() => {
     //const getMyNfts = async() => {
-    const getMyNfts = () => {
-      const url = 'https://testnets-api.opensea.io/assets?asset_contract_address=0x104ADFE3E1952d382575D7Af8CE2467256188921&order_direction=asc';
-      
-      fetch(url).then(response => response.json()).then(openseaData => {
-        //console.log(openseaData.assets);
+    //const getMyNfts = () => {
+      //const url = 'https://testnets-api.opensea.io/assets?asset_contract_address=0x104ADFE3E1952d382575D7Af8CE2467256188921&order_direction=asc';
+      //const url = 'https://testnets-api.opensea.io/assets?asset_contract_address=0x104ADFE3E1952d382575D7Af8CE2467256188921&order_direction=asc&offset=0&limit=20';
+      //const url = 'https://testnets-api.opensea.io/api/v1/assets?asset_contract_address=0x104ADFE3E1952d382575D7Af8CE2467256188921&order_direction=asc&offset=0';
+
+      /*fetch(url).then(response => response.json()).then(openseaData => {
+        console.log(openseaData.assets);
         setPunkListData(openseaData.assets);
-      })
+      })*/
       
       //const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x104ADFE3E1952d382575D7Af8CE2467256188921&order_direction=asc');
       //console.log(openseaData.data.assets);
       //setPunkListData(openseaData.data.assets);
-    }
+    //}
 
-    return getMyNfts();
+    const options = {method: 'GET'};
+
+    fetch('https://testnets-api.opensea.io/assets?asset_contract_address=0x104ADFE3E1952d382575D7Af8CE2467256188921&order_direction=asc&offset=0&limit=20', options)
+    .then(response => response.json())
+    .then(response => {console.log(response); setPunkListData(response.assets);})
+    .catch(err => console.error(err));
+
+    //return getMyNfts();
   }, [])
 
   return (
