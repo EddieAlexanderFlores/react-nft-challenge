c32a9ba65dfe2cc1abb57bb52ccef8a95e2f4989
Cowboy Ed <eaflores1231@gmail.com>
2021-12-24 07:46:34 -0500

    last fetch

--- src/App.js
+++ src/App.js
@@ -12,30 +12,21 @@ function App() {
   const [selectedPunk, setSelectedPunk] = useState(0);
 
   useEffect(() => {
-    //const getMyNfts = async() => {
-    //const getMyNfts = () => {
-      //const url = 'https://testnets-api.opensea.io/assets?asset_contract_address=0x104ADFE3E1952d382575D7Af8CE2467256188921&order_direction=asc';
-      //const url = 'https://testnets-api.opensea.io/assets?asset_contract_address=0x104ADFE3E1952d382575D7Af8CE2467256188921&order_direction=asc&offset=0&limit=20';
-      //const url = 'https://testnets-api.opensea.io/api/v1/assets?asset_contract_address=0x104ADFE3E1952d382575D7Af8CE2467256188921&order_direction=asc&offset=0';
+    const getMyNfts = async() => {
+      const url = 'https://testnets-api.opensea.io/assets?asset_contract_address=0x104ADFE3E1952d382575D7Af8CE2467256188921&order_direction=asc';
+      const options = {method: 'GET'};
 
-      /*fetch(url).then(response => response.json()).then(openseaData => {
-        console.log(openseaData.assets);
-        setPunkListData(openseaData.assets);
-      })*/
-      
-      //const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x104ADFE3E1952d382575D7Af8CE2467256188921&order_direction=asc');
-      //console.log(openseaData.data.assets);
-      //setPunkListData(openseaData.data.assets);
-    //}
+      fetch(url, options)
+      .then(response => response.json())
+      .then(openseaData => {console.log(openseaData.assets); setPunkListData(openseaData.assets);})
+      .catch(err => console.error(err));
+    
+      /*const openseaData = await axios.get(url);
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);*/
+    }
 
-    const options = {method: 'GET'};
-
-    fetch('https://testnets-api.opensea.io/assets?asset_contract_address=0x104ADFE3E1952d382575D7Af8CE2467256188921&order_direction=asc&offset=0&limit=20', options)
-    .then(response => response.json())
-    .then(response => {console.log(response); setPunkListData(response.assets);})
-    .catch(err => console.error(err));
-
-    //return getMyNfts();
+    return getMyNfts();
   }, [])
 
   return (
